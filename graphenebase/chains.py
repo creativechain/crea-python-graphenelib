0717f3ffdc42e4a28c9dd06567c551f74a88a195
ander7agar <ander7agar@gmail.com>
2018-09-17 20:51:32 +0200

    update chain id

--- graphenebase/chains.py
+++ graphenebase/chains.py
@@ -1,6 +1,6 @@
 default_prefix = "CREA"
 
-known_chains = {"CREA": {"chain_id": "38f14b346eb697ba04ae0f5adcfaa0a437ed3711197704aa256a14cb9b4a8f26",
+known_chains = {"CREA": {"chain_id": "0000000000000000000000000000000000000000000000000000000000000000",
                         "core_symbol": "CREA",
                         "prefix": "CREA"},
                 "TEST": {"chain_id": "39f5e2ede1f8bc1a3a54a7914414e3779e33193f1f5693510e73cb7a87617447",
